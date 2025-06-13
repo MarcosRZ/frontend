fe0a0cd3ef78b4504d0a7d0bb5f7b87470224297
MarcosRgz <msucaro@gmail.com>
2025-06-13 18:01:25 +0200

    Restyling bis

---
+++ src/app/components/index.ts
@@ -0,0 +1,1 @@
+export { default as SkullLogo } from './SkullLogo';
---
+++ src/app/propaganda.ts
@@ -0,0 +1,5 @@
+import localFont from 'next/font/local';
+export const propaganda = localFont({
+  src: './../../public/fonts/propaganda.ttf',
+  variable: '--font-propaganda',
+});
